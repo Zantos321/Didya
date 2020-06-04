9270e32cfb4159f14a70680484672a400f745fcc
Zantos321 <eljoe1985@gmail.com>
2020-06-04 10:26:30 -0700

    constructed router routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "./style/master.scss"; // applies global scss styles
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import HomePage from "./components/pages/Home";
+import AllTasks from "./components/pages/AllTasks";
+import EditTasks from "./components/pages/EditTasks";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-   return <></>;
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/all-tasks" component={AllTasks} />
+            <Route exact path="/edit-task" component={EditTasks} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
