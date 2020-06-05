54a6ac68a5470cc10303a558ea8801b658b1a8ff
Zantos321 <eljoe1985@gmail.com>
2020-06-04 18:11:03 -0700

    adda a filler background for testing

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -5,82 +5,83 @@ import { Link } from "react-router-dom";
 
 export default function Landing() {
    return (
-      <div className="container">
-         <Header />
-         <Navigation />
-         <h1 className="text-center">Landing Page</h1>
-         <div className="row">
-            <div>
-               <div className="card border-dark">
+      <div className="landing-background">
+         <div className="container-fluid">
+            <Header />
+            <Navigation />
+            <div className="card-deck mt-8">
+               <div className="card">
                   <div className="card-body">
-                     <h2 className="login-title">Login</h2>
-                     <p className="card-text">
-                        Login with your email address and password
-                     </p>
-                     <div className="form-group" id="login-form">
-                        <label className="input-text" htmlfor="email">
-                           Email Address
-                        </label>
-                        <input
-                           type="email"
-                           className="form-control"
-                           id="loginEmail"
-                        />
+                     <div className="landing-card">
+                        <h2 className="login-title">Login</h2>
+                        <p className="card-text">
+                           Login with your email address and password
+                        </p>
                         <div className="form-group" id="login-form">
-                           <label className="input-text" htmlFor="password">
-                              Password
+                           <label className="input-text" htmlfor="email">
+                              Email Address
                            </label>
                            <input
-                              type="password"
+                              type="email"
                               className="form-control"
-                              id="loginPassword"
+                              id="loginEmail"
                            />
+                           <div className="form-group" id="login-form">
+                              <label className="input-text" htmlFor="password">
+                                 Password
+                              </label>
+                              <input
+                                 type="password"
+                                 className="form-control"
+                                 id="loginPassword"
+                              />
+                           </div>
+                           <Link
+                              to="/all-tasks"
+                              id="loginVerify"
+                              className="btn btn-success btn-lg btn-landing btn-block"
+                           >
+                              Log in
+                           </Link>
                         </div>
-                        <Link
-                           to="/all-tasks"
-                           id="loginVerify"
-                           className="btn btn-success btn-lg btn-landing btn-block"
-                        >
-                           Log in
-                        </Link>
                      </div>
                   </div>
                </div>
-            </div>
 
-            <div>
-               <div className="card border-dark">
+               <div className="card">
                   <div className="card-body">
-                     <h2 className="signup-title">Sign Up</h2>
-                     <p className="card-text">
-                        Sign Up with your email address and password
-                     </p>
-                     <div className="form-group" id="signup-form">
-                        <label className="input-text" htmlfor="email">
-                           Email Address
-                        </label>
-                        <input
-                           type="email"
-                           className="form-control"
-                           id="signupEmail"
-                        />
+                     <div className="landing-card">
+                        <h2 className="signup-title">Sign Up</h2>
+                        <p className="card-text">
+                           Sign Up with your email address and password
+                        </p>
                         <div className="form-group" id="signup-form">
-                           <label className="input-text" htmlFor="password">
-                              Password
+                           <label className="input-text" htmlfor="email">
+                              Email Address
                            </label>
                            <input
-                              type="password"
+                              type="email"
                               className="form-control"
-                              id="signupPassword"
+                              id="signupEmail"
                            />
+                           <div className="form-group" id="signup-form">
+                              <label className="input-text" htmlFor="password">
+                                 Password
+                              </label>
+                              <input
+                                 type="password"
+                                 className="form-control"
+                                 id="signupPassword"
+                              />
+                           </div>
+                           <Link
+                              to="/all-tasks"
+                              id="signUpVerify"
+                              className="btn btn-success btn-lg btn-landing btn-block"
+                           >
+                              Sign Up
+                           </Link>
                         </div>
-                        <Link
-                           to="/all-tasks"
-                           id="signUpVerify"
-                           className="btn btn-success btn-lg btn-landing btn-block"
-                        >
-                           Sign Up
-                        </Link>
                      </div>
                   </div>
                </div>
