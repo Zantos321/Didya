c724cd55fd37e6f2c69ad1974dee8fbef97974a1
Zantos321 <eljoe1985@gmail.com>
2020-06-18 10:54:16 -0700

    made the add task button

--- src/components/pages/AllTasks.js
+++ src/components/pages/AllTasks.js
@@ -3,14 +3,17 @@ import BodyTemplate from "../ui/BodyTemplate";
 import { Link } from "react-router-dom";
 import TaskCard from "../ui/taskCard";
 import tasks from "../../mock-data/tasks";
+import { AddTask } from "../../icons/icons";
 
 export default function AllTasks() {
    return (
       <BodyTemplate>
          <div className="row col d-flex justify-content-between">
             <h1 className="pl-2">All Tasks</h1>
-            <Link to="/add-task">
-               <addTask />
+
+            <Link to="/add-task" className="mt-3 d-inline">
+               <AddTask className="addTaskIcon" />
+               <p className="addLinkText ml-1 pt-2 d-inline">Add Task</p>
             </Link>
          </div>
          {tasks.map((task) => {
