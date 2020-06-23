1be192c28b11361e930a29309b633f011aa0640d
Zantos321 <eljoe1985@gmail.com>
2020-06-22 17:06:54 -0700

    started work on mock adat and redux

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,59 +1,90 @@
 import React from "react";
+import axios from "axios";
 import BodyTemplate from "../ui/BodyTemplate";
 import diyaDots from "../../images/DIDYA-Logo-PURPLE-dotdotdot.svg";
 import { YesIcon } from "../../icons/Icons";
 import { NoIcon } from "../../icons/Icons";
 import tasks from "../../mock-data/tasks";
 import _ from "lodash";
+import { render } from "node-sass";
+import { bindActionCreators } from "redux";
 
+const mocky = "https://run.mocky.io/v3/e50b161e-7560-4806-b703-39c90750ecbb";
 // shuffles task list and pulls a task
 function selectTask() {
    const filteredTasks = tasks.filter((task) => !task.taskCompleted);
    return _.shuffle(filteredTasks)[0];
 }
 
-export default function HomePage() {
-   // pulls the initial tasks and sets its state
-   const [currentTask, setCurrentTask] = React.useState(selectTask());
-
-   function updateTask(userAccepted) {
-      // updates task completed to true and chooses a random task, otherwise chooses a random task
-      if (userAccepted) {
-         const taskIndex = tasks.findIndex(
-            (task) => task.id === currentTask.id
-         );
-         tasks[taskIndex].taskCompleted = true;
-      }
-      // resets all tasks if all completed
-      if (tasks.every((task) => task.taskCompleted)) {
-         for (const task of tasks) {
-            task.taskCompleted = false;
-         }
-      }
-      // gets a new current task
-      setCurrentTask(selectTask());
+class HomePage extends React.Component {
+   constructor(props) {
+      super(props);
+      axios
+         .get("https://run.mocky.io/v3/e50b161e-7560-4806-b703-39c90750ecbb")
+         .then(function (res) {
+            // handle success
+            console.log(res);
+            props.dispatch({
+               type: 
+            })
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         })
+         .finally(function () {
+            // always executed
+         });
    }
 
-   return (
-      <BodyTemplate>
-         <div className="row col">
-            <img src={diyaDots} alt="Didya..." className="ml-1 mt-4" />
-         </div>
-         <div className="row col-12 justify-content-center mb-4">
-            <div className="card didyaCard col-8 mb-4">
-               <div className="card-body">
-                  <p className="card-text didyaText">{currentTask.userTask}</p>
+
+   // pulls the initial tasks and sets its state
+   // const [currentTask, setCurrentTask] = React.useState(selectTask());
+
+   // function updateTask(userAccepted) {
+   //    // updates task completed to true and chooses a random task, otherwise chooses a random task
+   //    if (userAccepted) {
+   //       const taskIndex = tasks.findIndex(
+   //          (task) => task.id === currentTask.id
+   //       );
+   //       tasks[taskIndex].taskCompleted = true;
+   //    }
+   //    // resets all tasks if all completed
+   //    if (tasks.every((task) => task.taskCompleted)) {
+   //       for (const task of tasks) {
+   //          task.taskCompleted = false;
+   //       }
+   //    }
+   //    // gets a new current task
+   //    setCurrentTask(selectTask());
+   // }
+
+   render() {
+      return (
+         <BodyTemplate>
+            <div className="row col">
+               <img src={diyaDots} alt="Didya..." className="ml-1 mt-4" />
+            </div>
+            <div className="row col-12 justify-content-center mb-4">
+               <div className="card didyaCard col-8 mb-4">
+                  <div className="card-body">
+                     <p className="card-text didyaText">
+                        {currentTask.userTask}
+                     </p>
+                  </div>
                </div>
             </div>
-         </div>
-         <div className="row d-flex justify-content-around">
-            <button className="yesButton" onClick={() => updateTask(true)}>
-               <YesIcon className="yesIcon" />
-            </button>
-            <button className="noButton" onClick={() => updateTask()}>
-               <NoIcon className="noIcon" />
-            </button>
-         </div>
-      </BodyTemplate>
-   );
+            <div className="row d-flex justify-content-around">
+               <button className="yesButton" onClick={() => updateTask(true)}>
+                  <YesIcon className="yesIcon" />
+               </button>
+               <button className="noButton" onClick={() => updateTask()}>
+                  <NoIcon className="noIcon" />
+               </button>
+            </div>
+         </BodyTemplate>
+      );
+   }
 }
+
+export default HomePage;
